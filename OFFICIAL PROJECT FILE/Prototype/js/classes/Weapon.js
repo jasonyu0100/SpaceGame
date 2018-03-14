07c7b4a96b853de7a2fbbbb156b6ab79a0870802
Piploop <james.lee9112000@gmail.com>
2018-03-14 22:10:26 +1100

    Added Weapon Class

---
+++ OFFICIAL PROJECT FILE/Prototype/js/classes/Weapon.js
@@ -0,0 +1,10 @@
+class Weapon(){
+  constructor(bullet){ //bullet is a reference to the bullet class
+    this.bullet = bullet;
+  }
+
+  shoot(pos){
+    // Implement Dynamic Class and Proxy class
+    // https://stackoverflow.com/questions/34655616/create-an-instance-of-a-class-in-es6-with-a-dynamic-name
+  }
+}
