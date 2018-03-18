34ca6f829904a31b36cdb00ba586c9c4f2e3dc8d
huskyT <rex0355@gmail.com>
2018-03-18 19:44:03 +1100

    added pseudocode to shotgun.js

--- OFFICIAL PROJECT FILE/Prototype/js/classes/weapons/shotgun.js
+++ OFFICIAL PROJECT FILE/Prototype/js/classes/weapons/shotgun.js
@@ -2,8 +2,11 @@ class Shotgun extends Weapon {
     constructor() {
         super(aa) //Weapon constructor
     }
-    
+
     shoot(){
         super.shoot()
+        //Get the vector of the spaceship facing rn currently xd
+        //Create a new bullet with the same vector but...
+        //Add an angle to the bullet random(-angle, angle)
     }
 }
