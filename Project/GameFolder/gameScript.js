4aa35a64d45c62c7ca3bb86367e42f46eed80d73
Jason Yu <jasonyu0100@gmail.com>
2018-03-25 21:37:41 +1100

    Shapes

--- Project/GameFolder/gameScript.js
+++ Project/GameFolder/gameScript.js
@@ -20,8 +20,17 @@ document.body.appendChild(app.view)
 
 
 let mousePosition;
+let graphics = new PIXI.Graphics();
+graphics.beginFill(0xFF0000);
+graphics.drawCircle(50,50,50);
+graphics.endFill();
+
+graphics.beginFill(0x0000FF);
+graphics.drawPolygon([new PIXI.Point(100,100),new PIXI.Point(200,200),new PIXI.Point(300,100)]);
+graphics.endFill();
 function setup() {
 	mouseposition = app.renderer.plugins.interaction.mouse.global;
+	app.stage.addChild(graphics);
 }
 
 function gameLoop(delta) {
